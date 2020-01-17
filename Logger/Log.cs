b483a0a2d9d4da30dbedb91bff444ac9e26c0c63
pankajbstech <49518338+pankajbstech@users.noreply.github.com>
2020-01-17 10:41:59 +0530

    DesingPattern

---
+++ Logger/Log.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class Log : ILog
+    {
+        private Log()
+        {
+
+        }
+
+        public void LogException(string Message)
+        {
+            string fileName = string.Format("{0}_{1}.log", "Exception", DateTime.Now.ToShortDateString());
+            string logfilePath = string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, fileName);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("-------------------------------------------------------------------------------");
+            sb.AppendLine(DateTime.Now.ToString());
+            sb.AppendLine(Message);
+            using (StreamWriter writer = new StreamWriter(logfilePath, true))
+            {
+                writer.Write(sb.ToString());
+                writer.Flush();
+            }
+        }
+    }
+}
